f9a2145cbd06e928099cb78383905cb44b27f0f3
AlejandroDel <alejandrohdzd@proton.me>
2025-09-01 16:08:18 -0600

    Update task4.js

--- task4.js
+++ task4.js
@@ -1,5 +1,14 @@
 // Refer to Task 4 in your Instructions to complete this task
-
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Four!");
-  };
+// Misma situación quen el Task 3, aqui solo se le agrega el multiplo de 7, respetando del mas alto al mas bajo
+for (let i = 1; i < 106; i++) {
+    if (i % 15 == 0)
+        console.log("FizzBuzz");
+    else if (i % 7 == 0)
+        console.log("Woof");
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    else if (i % 3 == 0)
+        console.log("Fizz");
+    else
+        console.log(i);
+}
