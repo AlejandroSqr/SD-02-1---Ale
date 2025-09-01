e55c4da92721426db1a9562ce72aef0b23daf9fd
AlejandroDel <alejandrohdzd@proton.me>
2025-09-01 16:05:42 -0600

    Update task2.js

--- task2.js
+++ task2.js
@@ -1,5 +1,10 @@
 // Refer to Task 2 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Two!");
-  };
+//Igual que en ejercicio 1, Variable "i"
+//Al solo querer un divisible entre 3, solo se pone "Fizz" en la lista de 1 a 105
+for (let i = 1; i < 106; i++) {
+    if (i % 3 == 0)
+        console.log("Fizz");
+    else
+        console.log(i);
+}
