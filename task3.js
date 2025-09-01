37173b9157b8b5d335591e82d14ebfc051ad6df9
AlejandroDel <alejandrohdzd@proton.me>
2025-09-01 16:07:08 -0600

    Update task3.js

--- task3.js
+++ task3.js
@@ -1,5 +1,13 @@
 // Refer to Task 3 in your Instructions to complete this task
 
-for (let i = 0; i < 1; i++) {
-    console.log("This is Task Three!");
-  };
+//Aqui cambia un poco la cosa, como se pide que este Fizz, Buzz y FizzBuzz en la lista del 1 a 105, es editor recomienda empezar desde el multiplo mas alto y llegar al mas bajo
+for (let i = 1; i < 106; i++) {
+    if (i % 15 == 0)
+        console.log("FizzBuzz");
+    else if (i % 5 == 0)
+        console.log("Buzz");
+    else if (i % 3 == 0)
+        console.log("Fizz");
+    else
+        console.log(i);
+}
